0cc1a72e03c133367d4808709cb916fd99375b93
Jeff <Jeff@DESKTOP-IR1ETOM>
2024-01-20 21:43:47 -0500

    Add project files.

---
+++ SubReddit/Program.cs
@@ -0,0 +1,33 @@
+﻿using SubRedditLogic;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SubReddit
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            var ServiceProvider = BuildServiceProvider();
+
+            
+            Interval I = new Interval();
+            I.Setup();
+            I.Run();
+
+            //ServiceProvider.GetRequiredService<IInterval>().Setup();
+            //ServiceProvider.GetRequiredService<IInterval>().Run();
+
+        }
+
+        private static IServiceProvider BuildServiceProvider()
+        {
+            return new ServiceCollection()
+                .AddTransient<IInterval,Interval>()
+                .BuildServiceProvider();
+
+        }
+    }
+}
---
+++ SubRedditLogic/Interval.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace SubRedditLogic
+{
+    public class Interval : IInterval
+    {
+        private static System.Timers.Timer timer;
+        private static SubRedditLogic redditLogic;
+        private readonly ILogger<Interval> logger;
+        private readonly ILogger<SubRedditLogic> subredditlogger;
+        IServiceProvider provider = null;
+
+        public Interval(ILogger<Interval> _logger)
+        {
+            logger = _logger;
+
+            provider = new ServiceCollection()
+                .AddTransient<ISubRedditLogic, SubRedditLogic>()
+                .BuildServiceProvider();
+        }
+
+        public Interval()
+        { }
+        public void Setup()
+        {
+            
+
+            redditLogic = new SubRedditLogic(subredditlogger);
+            redditLogic.Setup();
+
+
+        }
+        public void Run()
+        {
+            Timer timer = new Timer(TimerCallback, null, TimeSpan.Zero, TimeSpan.FromSeconds(5));
+
+
+            // Keep the program running
+            Console.WriteLine("Press Enter to exit.");
+            Console.ReadLine();
+        }
+
+        
+
+
+        private static void TimerCallback(object state)
+        {
+            // Call the Process() method at the specified interval
+            redditLogic.Process();
+
+        }
+    }
+}
